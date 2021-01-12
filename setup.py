e2774b9f862e6b39d2b9b0e46a29c7b7a4d18a1a
flych3r <xavier_sampaio@alu.ufc.br>
2021-01-12 15:07:14 -0300

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ DEPENDENCIES = [
 
 setup(
     name='pymove-osmnx',
-    version='0.0.1',
+    version='0.2.0',
     author='Insight Data Science Lab',
     author_email='insightlab@dc.ufc.br',
     license='MIT',
